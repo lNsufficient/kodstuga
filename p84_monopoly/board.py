052d374ab8e1526a2e08e0cf814538d9aa3bf9a2
Edvard Johansson <edvard.jo@gmail.com>
2019-10-12 12:59:34 +0200

    dice steps

--- p84_monopoly/board.py
+++ p84_monopoly/board.py
@@ -1,7 +1,14 @@
+import random
+
 class Board(object):
     def __init__():
         board_list = [0]*40
         current_position = 0
+
+    def get_next_dice():
+        d1 = random.randint(1, 6)
+        d2 = random.randint(1, 6)
+        return d1+d2   
 
     def walk(self):
         steps = self.get_next_dice()
