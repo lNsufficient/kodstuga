dd5d1ed2fefe7cc85ea8ce2b19f64c3bef72ed39
Edvard Johansson <edvard.jo@gmail.com>
2019-10-12 14:09:55 +0200

    added board tiles

--- p84_monopoly/board.py
+++ p84_monopoly/board.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 class Board(object):
     def __init__(self):
@@ -6,6 +7,12 @@ class Board(object):
         self.current_position = 0
         self.board_len = len(self.board_list)
         self.round = 0
+        self.board_tiles = [
+                "GO", "A1", "CC1","A2", "T1","R1", "B1", "CH1", "B2", "B3", 
+                "JAIL","C1","U1", "C2", "C3", "R2", "D1", "CC2", "D2", "D3", 
+                "FP", "E1", "CH2", "E2", "E3", "R3", "F1", "F2", "U2", "F3",
+                "G2J", "G1", "G2", "CC3", "G3", "R4", "CH3", "H1", "T2", "H2"
+                ]                
 
     @staticmethod
     def get_next_dice():
@@ -35,7 +42,7 @@ class Board(object):
 
 if __name__ == "__main__":
     b = Board()
-    rounds = 100000
+    rounds = int(sys.argv[1])
     while(b.round < rounds):
         b.walk()
     print(b)
