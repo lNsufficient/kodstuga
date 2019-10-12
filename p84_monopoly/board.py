c7a941642a87ac3eea0c63b8a561628c8787de70
Edvard Johansson <edvard.jo@gmail.com>
2019-10-12 13:00:33 +0200

    dist steps staticmethod

--- p84_monopoly/board.py
+++ p84_monopoly/board.py
@@ -5,13 +5,14 @@ class Board(object):
         board_list = [0]*40
         current_position = 0
 
+    @staticmethod
     def get_next_dice():
         d1 = random.randint(1, 6)
         d2 = random.randint(1, 6)
         return d1+d2   
 
     def walk(self):
-        steps = self.get_next_dice()
+        steps = Board.get_next_dice()
         self.current_position = self.get_next_position(steps)
         self.current_position = self.take_action()
         self.board_list[self.current_position] = self.board_list[self.current_position] + 1
