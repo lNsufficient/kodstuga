24bd019857b4466601ba928e406c1283b1688faa
Edvard Johansson <edvard.jo@gmail.com>
2019-10-12 14:17:28 +0200

    first implementation of take_action

--- p84_monopoly/board.py
+++ p84_monopoly/board.py
@@ -24,8 +24,18 @@ class Board(object):
         next_position = (self.current_position + steps)%self.board_len
         return next_position
 
-    def take_action(self):
-        return self.current_position
+    def take_cc(self, position):
+        #returns new position tile
+
+    def take_action(self, position):
+        tile = self.board_tiles[position]
+        if tile == "G2J":
+            new_position = goto("JAIL")
+        if tile[0:2] == "CC":
+            new_position = goto(take_cc(position))
+        if tile[0:2] == "CH":
+            new_position = goto(take_ch(position))
+        return new_position
 
     def walk(self):
         steps = Board.get_next_dice()
